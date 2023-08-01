5b711b967beadea4a3b596e72230736fc92974bb
Eren <67120341+ErenKCKL@users.noreply.github.com>
2023-08-01 17:40:35 +0200

    Add files via upload

---
+++ ss_c3_e1/src/main/java/com/example/ss_c3_e1/config/SecurityConfig.java
@@ -0,0 +1,32 @@
+package com.example.ss_c3_e1.config;
+
+import com.example.ss_c3_e1.config.security.filters.CustomAuthenticationFilter;
+import lombok.AllArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@AllArgsConstructor
+public class SecurityConfig {
+
+    private final CustomAuthenticationFilter customAuthenticationFilter;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+        return http
+                .addFilterAt(customAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
+                .authorizeRequests().anyRequest().authenticated()// don't worry about this line
+                .and().build();
+    }
+
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
+}
